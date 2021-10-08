c4fbcd3ad2193c773518b003f984c6fe8954cdae
R3X-G1L6AME5H <r3x_g1l6ame5h@protonmail.com>
2021-10-08 23:21:57 +0200

    fixed the load on start issue

--- addons/LOD_Manager/LODManager.gd
+++ addons/LOD_Manager/LODManager.gd
@@ -7,78 +7,118 @@ export (bool) var build = false
 export (bool) var track = false setget _set_track
 export (bool) var fade = false
 
-export (float, 0.01, 100) var default_lod1 = 4
-export (float, 0.01, 100) var default_lod2 = 8
-export (float, 0.01, 100) var default_lod3 = 16
-export (float, 0.0, 1.0)  var fade_in_delay = 0.5
+var default_lod_1 = 4
+var default_lod_2 = 8
+var default_lod_3 = 16 
+export (float, 0.0, 1.0)var fade_in_delay = 0.5
 
 var chunk_web = {}
 var web_built = false
 
-func _process(_delta):
-	if build:
-		build = false
-		web_built = false
+func _get( property : String ):
+	match property:
+		"default_lod/default_lod_1":
+			return default_lod_1
+		"default_lod/default_lod_2":
+			return default_lod_2
+		"default_lod/default_lod_3":
+			return default_lod_3 
+		_ :
+			return null
+
+func _set( property : String, value ) -> bool:
+	match property:
+		"default_lod/default_lod_1":
+			default_lod_1 = float(value)
+			return true
+		"default_lod/default_lod_2":
+			default_lod_2 = float(value)
+			return true
+		"default_lod/default_lod_3":
+			default_lod_3 = float(value)
+			return true
+		_ :
+			return false
+
+func _get_property_list():
+	var result = []
+	result.push_back({
+			"name": "default_lod/default_lod_1",
+			"type": TYPE_REAL,
+			"usage": PROPERTY_USAGE_SCRIPT_VARIABLE | PROPERTY_USAGE_DEFAULT,
+			"hint": PROPERTY_HINT_RANGE,
+			"hint_string": "0.01,100,0.01,or_greater"
+	})
+	result.push_back({
+			"name": "default_lod/default_lod_2",
+			"type": TYPE_REAL,
+			"usage": PROPERTY_USAGE_SCRIPT_VARIABLE | PROPERTY_USAGE_DEFAULT,
+			"hint": PROPERTY_HINT_RANGE,
+			"hint_string": "0.01,100,0.01,or_greater"
+	})
+	result.push_back({
+			"name": "default_lod/default_lod_3",
+			"type": TYPE_REAL,
+			"usage": PROPERTY_USAGE_SCRIPT_VARIABLE | PROPERTY_USAGE_DEFAULT,
+			"hint": PROPERTY_HINT_RANGE,
+			"hint_string": "0.01,100,0.01,or_greater"
+	})
+	return result
+
+func _ready():
+	property_list_changed_notify()
+	if Engine.editor_hint:
+		pass
+	else:
 		chunk_web = _build_chunk_web(self)
 		web_built = true
+
+func _process(_delta):
+	if Engine.editor_hint:
+		if build:
+			build = false
+			web_built = false
+			chunk_web = _build_chunk_web(self)
+			web_built = true
 	
-	if web_built and track:
-		_scan_chunk(chunk_web)
+		if web_built and track:
+			_scan_chunk(chunk_web)
+	else:
+		if web_built:
+			_scan_chunk(chunk_web)
 
 func _scan_chunk( dict : Dictionary) -> void:
 		var pos : Vector3
 		for key in dict.keys():
-			
-			if Engine.editor_hint:
-				if not track_target:
-					track = false
-					return
+			if track_target:
 				pos = get_node(track_target).global_transform.origin
-			
 			else:
-				if track_target:
-					pos = get_node(track_target).global_transform.origin
-				else:
-					pos = get_viewport().get_camera().global_transform.origin
+				pos = get_viewport().get_camera().global_transform.origin
 			
 			var dist : float = (dict[key].position - pos).length()
 			
-			
-			if fade:
-				### LOD 1 TRANSPARENCY
-				dict[key].meshes[0].visible = 0 < dist and dist < dict[key].borders[1]
-				
-				
-				### LOD 2 TRANSPARENCY
-				var tmp
-				if dict[key].meshes[1]: 
-					tmp =   clamp(\
-								1.0 - (dist                      - dict[key].borders[1])\
-									/ (lerp(dict[key].borders[0], dict[key].borders[1], fade_in_delay) - dict[key].borders[1]), \
-								0.0, 1.0 )\
-							*    float(dist < dict[key].borders[2])
-					dict[key].meshes[1].visible = tmp != 0
-					dict[key].meshes[1].get("material/0").albedo_color.a = tmp
-				
-				### LOD 3 TRANSPARENCY
-				if dict[key].meshes[2]:
-					tmp =   clamp(\
-								1.0 - (dist                      - dict[key].borders[2])\
-									/ (lerp(dict[key].borders[1], dict[key].borders[2], fade_in_delay) - dict[key].borders[2]), \
-								0.0, 1.0 )
-					dict[key].meshes[2].visible = tmp != 0
-					dict[key].meshes[2].get("material/0").albedo_color.a = tmp
-			
-			else:
-				dict[key].meshes[0].visible = 0 < dist and dist < dict[key].borders[0]
-				if dict[key].meshes[1]:
-					dict[key].meshes[1].visible = dict[key].borders[0] < dist and dist < dict[key].borders[1]
-				if dict[key].meshes[2]:
-					dict[key].meshes[2].visible = dict[key].borders[1] < dist and dist < dict[key].borders[2]
+			_toggle_chunk( dict[key].meshes[0],  0                    < dist and dist <= dict[key].borders[0])
+			_toggle_chunk( dict[key].meshes[1],  dict[key].borders[0] < dist and dist <= dict[key].borders[1])
+			_toggle_chunk( dict[key].meshes[2],  dict[key].borders[1] < dist and dist <= dict[key].borders[2])
 			
 			## It will process the children only if the chunk is LOD 1 for performance reasons
 			if dict[key].has("children") and dict[key].meshes[0].visible:
 				_scan_chunk(dict[key].children)
+
+func _toggle_chunk( chunk, toggle ):
+	if chunk.visible != toggle:
+		chunk.visible = toggle
+		chunk.set_process(toggle)
+		chunk.set_physics_process(toggle)
+		chunk.set_process_unhandled_input(toggle)
+		chunk.set_process_input(toggle)
+	
+		### Disable collisions wherever possible
+		for child in chunk.get_children():
+			if child is PhysicsBody:
+				for collision in child.get_children():
+					if collision is CollisionShape:
+						collision.set_deferred("disabled", not toggle)
 
 func _build_chunk_web( target ) -> Dictionary:
 	var graph = {}
@@ -96,14 +136,14 @@ func _build_chunk_web( target ) -> Dictionary:
 				if target.has_node(instance + "-lod3"):
 					mesh_2 = target.get_node(instance + "-lod3")
 				
-				var border_1 = default_lod1
-				var border_2 = default_lod2
-				var border_3 = default_lod3
+				var border_1 = default_lod_1
+				var border_2 = default_lod_2
+				var border_3 = default_lod_3
 				
 				if child.get("LOD_PLUGIN_ID") == 1:
-					border_1 = child.get("plugin_lod/lod_1")
-					border_2 = child.get("plugin_lod/lod_2")
-					border_3 = child.get("plugin_lod/lod_3")
+					border_1 = child.get("default_lod/default_lod_1")
+					border_2 = child.get("default_lod/default_lod_2")
+					border_3 = child.get("default_lod/default_lod_3")
 					
 				
 				graph[instance] = {
@@ -121,7 +161,7 @@ func _set_track(val) -> void:
 	if not val:
 		if web_built:
 			_clean_up(chunk_web)
-			
+
 func _clean_up( dict : Dictionary ) -> void:
 	for key in dict.keys():
 		dict[key].meshes[0].visible = dict[key].init_visibility
@@ -132,3 +172,32 @@ func _clean_up( dict : Dictionary ) -> void:
 		
 		if dict[key].has("children"):
 			_clean_up(dict[key].children)
+
+
+"""
+if fade:
+				### LOD 1 TRANSPARENCY
+				dict[key].meshes[0].visible = 0 < dist and dist < dict[key].borders[1]
+				
+				
+				### LOD 2 TRANSPARENCY
+				var tmp
+				if dict[key].meshes[1]: 
+					tmp =   clamp(
+								1.0 - (dist                      - dict[key].borders[1])
+									/ (lerp(dict[key].borders[0], dict[key].borders[1], fade_in_delay) - dict[key].borders[1]), 
+								0.0, 1.0 )
+							*    float(dist < dict[key].borders[2])
+					dict[key].meshes[1].visible = tmp != 0
+					dict[key].meshes[1].get("material/0").albedo_color.a = tmp
+				
+				### LOD 3 TRANSPARENCY
+				if dict[key].meshes[2]
+					tmp =   clamp(
+								1.0 - (dist                      - dict[key].borders[2])
+									/ (lerp(dict[key].borders[1], dict[key].borders[2], fade_in_delay) - dict[key].borders[2]), 
+								0.0, 1.0 )
+					dict[key].meshes[2].visible = tmp != 0
+					dict[key].meshes[2].get("material/0").albedo_color.a = tmp
+			
+"""
