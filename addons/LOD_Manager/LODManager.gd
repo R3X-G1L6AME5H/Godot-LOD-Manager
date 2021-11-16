51d3f133ccecd120971216b6bd31a315cd5e3481
R3X-G1L6AME5H <r3x_g1l6ame5h@protonmail.com>
2021-11-16 18:29:22 +0100

    Remodeled the LODManager

--- addons/LOD_Manager/LODManager.gd
+++ addons/LOD_Manager/LODManager.gd
@@ -1,111 +1,120 @@
 tool
 extends Spatial
-const LOD_PLUGIN_ID = 0
 
-export (NodePath) var track_target = null 
-export (bool) var build = false
-export (bool) var track = false setget _set_track
-export (bool) var fade = false
+const LOD_OBJECT = preload("res://addons/LOD_Manager/LODObject.gd")
+export (bool) var group_levels_of_distance = false setget _group_levels_of_distance
+export (bool) var enable = false setget _enable
 
-var default_lod_1 = 4
-var default_lod_2 = 8
-var default_lod_3 = 16 
-export (float, 0.0, 1.0)var fade_in_delay = 0.5
+export (NodePath) var tracking_target = null setget _set_target
+var target_node : Node = null
+var PLAYER_POSITION := Vector3.ZERO
 
-var chunk_web = {}
-var web_built = false
+"""
+HANDLE BUTTON PRESS FOR REOGANIZING MESHES
+"""
+func _reparent_node(node_to_reparent, new_parent):
+	var old_parent = node_to_reparent.get_parent()
+	old_parent.remove_child(node_to_reparent)
+	new_parent.add_child(node_to_reparent)
+	_set_owner_for_node_and_children(node_to_reparent, new_parent.get_owner())
 
-func _get( property : String ):
-	match property:
-		"default_lod/default_lod_1":
-			return default_lod_1
-		"default_lod/default_lod_2":
-			return default_lod_2
-		"default_lod/default_lod_3":
-			return default_lod_3 
-		_ :
-			return null
+func _set_owner_for_node_and_children(node, owner):
+	node.set_owner(owner)
+	for child_node in node.get_children():
+		_set_owner_for_node_and_children(child_node, owner)
 
-func _set( property : String, value ) -> bool:
-	match property:
-		"default_lod/default_lod_1":
-			default_lod_1 = float(value)
-			return true
-		"default_lod/default_lod_2":
-			default_lod_2 = float(value)
-			return true
-		"default_lod/default_lod_3":
-			default_lod_3 = float(value)
-			return true
-		_ :
-			return false
+func _group_levels_of_distance(val):
+	group_levels_of_distance = val
+	if val:
+		var current_node = find_node("*-lod1", false)
+		var counter = 0
+		
+		while current_node:
+			### CREATING THE KNOT
+			var node = Spatial.new()
+			node.name = current_node.name.trim_suffix("-lod1")
+			self.add_child(node, true)
+			node.set_owner(self)
+			node.set_script(LOD_OBJECT)
+			
+			## take old node's position
+			node.global_transform = current_node.global_transform
+			
+			### REORGANISING THE NODES
+			_reparent_node(current_node, node)
+			current_node.transform = Transform()
+			
+			var lod_node = find_node(node.name + "-lod2", false)
+			if lod_node:
+				_reparent_node(lod_node, node)
+				lod_node.transform = Transform()
+			
+			lod_node = find_node(node.name + "-lod3", false)
+			if lod_node:
+				_reparent_node(lod_node, node)
+				lod_node.transform = Transform()
+			
+			counter += 1
+			current_node = find_node("*-lod1", false)
+		
+		print("LOD Manager created ", str(counter), " new knots.")
 
-func _get_property_list():
-	var result = []
-	result.push_back({
-			"name": "default_lod/default_lod_1",
-			"type": TYPE_REAL,
-			"usage": PROPERTY_USAGE_SCRIPT_VARIABLE | PROPERTY_USAGE_DEFAULT,
-			"hint": PROPERTY_HINT_RANGE,
-			"hint_string": "0.01,100,0.01,or_greater"
-	})
-	result.push_back({
-			"name": "default_lod/default_lod_2",
-			"type": TYPE_REAL,
-			"usage": PROPERTY_USAGE_SCRIPT_VARIABLE | PROPERTY_USAGE_DEFAULT,
-			"hint": PROPERTY_HINT_RANGE,
-			"hint_string": "0.01,100,0.01,or_greater"
-	})
-	result.push_back({
-			"name": "default_lod/default_lod_3",
-			"type": TYPE_REAL,
-			"usage": PROPERTY_USAGE_SCRIPT_VARIABLE | PROPERTY_USAGE_DEFAULT,
-			"hint": PROPERTY_HINT_RANGE,
-			"hint_string": "0.01,100,0.01,or_greater"
-	})
-	return result
 
-func _ready():
-	property_list_changed_notify()
-	if Engine.editor_hint:
-		pass
-	else:
-		chunk_web = _build_chunk_web(self)
-		web_built = true
+"""
+MANAGE ACTIVATION
+"""
+func _enable(val):
+	enable = val
+	set_process(enable)
+
+func _set_target(val):
+	if val:
+		if get_node_or_null(val):
+			print("Node Exists")
+			if get_node(val).get("global_transform") != null:
+				print("Node has a position")
+				tracking_target = val
+				target_node = get_node(val)
+			else:
+				tracking_target = null
+				target_node = null
+		else:
+			tracking_target = null
+			target_node = null
 
 func _process(_delta):
-	if Engine.editor_hint:
-		if build:
-			build = false
-			web_built = false
-			chunk_web = _build_chunk_web(self)
-			web_built = true
-	
-		if web_built and track:
-			_scan_chunk(chunk_web)
+	if target_node:
+		PLAYER_POSITION = target_node.global_transform.origin
 	else:
-		if web_built:
-			_scan_chunk(chunk_web)
+		PLAYER_POSITION = Vector3.ZERO
+	
+	for child in self.get_children():
+		if child is LOD_OBJECT:
+			_scan_node_tree(child)
 
-func _scan_chunk( dict : Dictionary) -> void:
-		var pos : Vector3
-		for key in dict.keys():
-			if track_target:
-				pos = get_node(track_target).global_transform.origin
+"""
+RECURSIVELY TRAVERSE THE TREE ENABLING AND DISABLING NODES
+"""
+func _scan_node_tree( node : LOD_OBJECT ):
+	var distance : float = (node.global_transform.origin - PLAYER_POSITION).length_squared()
+	
+	for child in node.get_children():
+		if child is LOD_OBJECT:
+			_scan_node_tree( child )
+		else:
+			if child.name.ends_with("-lod1") and distance < node.lod_1 * node.lod_1 or\
+			   child.name.ends_with("-lod2") and node.lod_1 * node.lod_1 <= distance and distance < node.lod_2 * node.lod_2 or \
+			   child.name.ends_with("-lod3") and node.lod_2 * node.lod_2 <= distance and distance < node.lod_3 * node.lod_3:
+				_tick_chunk(child, true)
+				#prints(child.name, "- VISIBLE", distance)
 			else:
-				pos = get_viewport().get_camera().global_transform.origin
-			
-			var dist : float = (dict[key].position - pos).length()
-			
-			_toggle_chunk( dict[key].meshes[0],  0                    < dist and dist <= dict[key].borders[0])
-			_toggle_chunk( dict[key].meshes[1],  dict[key].borders[0] < dist and dist <= dict[key].borders[1])
-			_toggle_chunk( dict[key].meshes[2],  dict[key].borders[1] < dist and dist <= dict[key].borders[2])
-			
-			## It will process the children only if the chunk is LOD 1 for performance reasons
-			if dict[key].has("children") and dict[key].meshes[0].visible:
-				_scan_chunk(dict[key].children)
+				_tick_chunk(child, false)
+				#prints(child.name, "- HIDDEN -", distance)
 
-func _toggle_chunk( chunk, toggle ):
+"""
+ENABLE/DISABLE NODES
+"""
+func _tick_chunk( chunk, toggle ):
 	if chunk.visible != toggle:
 		chunk.visible = toggle
 		chunk.set_process(toggle)
@@ -120,84 +129,11 @@ func _toggle_chunk( chunk, toggle ):
 					if collision is CollisionShape:
 						collision.set_deferred("disabled", not toggle)
 
-func _build_chunk_web( target ) -> Dictionary:
-	var graph = {}
-	for child in target.get_children():
-		if child.has_method("set_visible"):
-			if child.name.ends_with("-lod1"):
-				var instance = child.name.trim_suffix("-lod1")
-				
-				## Get all LOD Meshes
-				var mesh_1 = null
-				if target.has_node(instance + "-lod2"):
-					mesh_1 = target.get_node(instance + "-lod2")
-				
-				var mesh_2 = null
-				if target.has_node(instance + "-lod3"):
-					mesh_2 = target.get_node(instance + "-lod3")
-				
-				var border_1 = default_lod_1
-				var border_2 = default_lod_2
-				var border_3 = default_lod_3
-				
-				if child.get("LOD_PLUGIN_ID") == 1:
-					border_1 = child.get("default_lod/default_lod_1")
-					border_2 = child.get("default_lod/default_lod_2")
-					border_3 = child.get("default_lod/default_lod_3")
-					
-				
-				graph[instance] = {
-					"position"        : child.global_transform.origin,
-					"init_visibility" : child.visible,
-					"borders"         : [ border_1,  border_2,  border_3],
-					"meshes"          : [ child,     mesh_1,    mesh_2 ],
-					"children"        : _build_chunk_web(child)
-				}
-	
-	return graph
-
-func _set_track(val) -> void:
-	track = val
-	if not val:
-		if web_built:
-			_clean_up(chunk_web)
-
-func _clean_up( dict : Dictionary ) -> void:
-	for key in dict.keys():
-		dict[key].meshes[0].visible = dict[key].init_visibility
-		if dict[key].meshes[1]:
-			dict[key].meshes[1].visible = false
-		if dict[key].meshes[2]:
-			dict[key].meshes[2].visible = false
-		
-		if dict[key].has("children"):
-			_clean_up(dict[key].children)
-
-
-"""
-if fade:
-				### LOD 1 TRANSPARENCY
-				dict[key].meshes[0].visible = 0 < dist and dist < dict[key].borders[1]
-				
-				
-				### LOD 2 TRANSPARENCY
-				var tmp
-				if dict[key].meshes[1]: 
-					tmp =   clamp(
-								1.0 - (dist                      - dict[key].borders[1])
-									/ (lerp(dict[key].borders[0], dict[key].borders[1], fade_in_delay) - dict[key].borders[1]), 
-								0.0, 1.0 )
-							*    float(dist < dict[key].borders[2])
-					dict[key].meshes[1].visible = tmp != 0
-					dict[key].meshes[1].get("material/0").albedo_color.a = tmp
-				
-				### LOD 3 TRANSPARENCY
-				if dict[key].meshes[2]
-					tmp =   clamp(
-								1.0 - (dist                      - dict[key].borders[2])
-									/ (lerp(dict[key].borders[1], dict[key].borders[2], fade_in_delay) - dict[key].borders[2]), 
-								0.0, 1.0 )
-					dict[key].meshes[2].visible = tmp != 0
-					dict[key].meshes[2].get("material/0").albedo_color.a = tmp
-			
-"""
+#### DEBUG ####
+#export (bool) var step = false setget _step
+#func _step(val):
+#	step = val
+#	if step == true:
+#		for child in self.get_children():
+#			if child is LOD_OBJECT:
+#				_scan_node_tree(child)
--- addons/LOD_Manager/LODObject.gd
+++ addons/LOD_Manager/LODObject.gd
@@ -2,65 +2,7 @@ tool
 extends Spatial
 const LOD_PLUGIN_ID = 1
 
-#### MANAGMENT VARIABLES
-var neibourghs = []
-
 #### EDITOR VARIABLES
-var lod_1 : float = 8
-var lod_2 : float = 16
-var lod_3 : float = 32
-
-var neibourgh_radius : float 
-
-func _get( property : String ):
-	match property:
-		"plugin_lod/lod_1":
-			return lod_1
-		"plugin_lod/lod_2":
-			return lod_2
-		"plugin_lod/lod_3":
-			return lod_3 
-		_ :
-			return null
-
-func _set( property : String, value ) -> bool:
-	match property:
-		"plugin_lod/lod_1":
-			lod_1 = float(value)
-			return true
-		"plugin_lod/lod_2":
-			lod_2 = float(value)
-			return true
-		"plugin_lod/lod_3":
-			lod_3 = float(value)
-			return true
-		_ :
-			return false
-
-func _get_property_list():
-	var result = []
-	result.push_back({
-			"name": "plugin_lod/lod_1",
-			"type": TYPE_REAL,
-			"usage": PROPERTY_USAGE_SCRIPT_VARIABLE | PROPERTY_USAGE_DEFAULT,
-			"hint": PROPERTY_HINT_RANGE,
-			"hint_string": "0.01,100,0.01,or_greater"
-	})
-	result.push_back({
-			"name": "plugin_lod/lod_2",
-			"type": TYPE_REAL,
-			"usage": PROPERTY_USAGE_SCRIPT_VARIABLE | PROPERTY_USAGE_DEFAULT,
-			"hint": PROPERTY_HINT_RANGE,
-			"hint_string": "0.01,100,0.01,or_greater"
-	})
-	result.push_back({
-			"name": "plugin_lod/lod_3",
-			"type": TYPE_REAL,
-			"usage": PROPERTY_USAGE_SCRIPT_VARIABLE | PROPERTY_USAGE_DEFAULT,
-			"hint": PROPERTY_HINT_RANGE,
-			"hint_string": "0.01,100,0.01,or_greater"
-	})
-	return result
-
-func _ready():
-	property_list_changed_notify()
+export (float) var lod_1 : float = 8
+export (float) var lod_2 : float = 16
+export (float) var lod_3 : float = 32
