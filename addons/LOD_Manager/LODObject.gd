4cd2eb41fd3cc186e709874d9beecbf8019a3395
R3X-G1L6AME5H <r3x.g1l6ame5h@protonmail.com>
2022-08-09 13:39:09 +0000

    Stamp&Document (pt1) - LOD Object

--- addons/LOD_Manager/LODObject.gd
+++ addons/LOD_Manager/LODObject.gd
@@ -1,5 +1,17 @@
 tool
 extends Spatial
+
+"""
+	Level-of-Detail Object 
+		by Nemo Czanderlitch/Nino Čandrlić	
+			@R3X-G1L       (godot assets store)
+			R3X-G1L6AME5H  (github)
+
+	A slave script to LOD Manager. This object holds the rules of rendering
+	used by the Manager. Namely, distances of each layer.
+"""
+
+
 const LOD_PLUGIN_ID = 1
 
 #### EDITOR VARIABLES
